b02e12d27420840285c9cac392728d9bc0fe8175
Brandon Liu <bdon@bdon.org>
2015-08-04 19:10:29 +0800

    add accessibility map

--- osm/osm_exits.py
+++ osm/osm_exits.py
@@ -3,7 +3,12 @@ import json
 features = []
 
 for e in json.loads(open('osm_exits.json').read())['elements']:
-  features.append({'type':'Feature','id':e['id'],'geometry':{'type':'Point','coordinates':[e['lon'],e['lat']]}})
+  features.append({
+    'type':'Feature',
+    'id':e['id'],
+    'geometry':{'type':'Point','coordinates':[e['lon'],e['lat']]},
+    'properties':e['tags']
+  })
 
 with open('osm_exits.geojson','w') as output:
   output.write(json.dumps({'type':'FeatureCollection','features':features}))
